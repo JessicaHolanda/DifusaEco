2edfa34106c4272dfb8634a43a54bbb717ed6bce
Raphael Scaziti <rscaziti@gmail.com>
2021-02-22 09:53:08 -0300

    Criado model - categoria

---
+++ src/main/java/com/difusaEco/DifusaEco/model/Categoria.java
@@ -0,0 +1,61 @@
+package com.difusaEco.DifusaEco.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 2, max = 50)
+	private String nomeCategoria;
+	
+	@NotNull
+	@Size(min = 2, max = 50)
+	private String subCategoria;
+	
+	private boolean ativo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+
+	public String getSubCategoria() {
+		return subCategoria;
+	}
+
+	public void setSubCategoria(String subCategoria) {
+		this.subCategoria = subCategoria;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+}

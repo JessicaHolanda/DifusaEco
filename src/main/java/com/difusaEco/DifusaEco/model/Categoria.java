fac21d15e6cc720caa912fe429cb1283ae449922
Denise Anjos <deniseb.anjos@outlook.com>
2021-03-01 10:29:55 -0300

    feat(Produto/Usuario):Criado model usuario e crud produto

--- src/main/java/com/difusaEco/DifusaEco/model/Categoria.java
+++ src/main/java/com/difusaEco/DifusaEco/model/Categoria.java
@@ -1,12 +1,18 @@
 package com.difusaEco.DifusaEco.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -25,6 +31,10 @@ public class Categoria
 	private String subCategoria;
 	
 	private boolean ativo;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public long getId() {
 		return id;
@@ -56,6 +66,14 @@ public class Categoria
 
 	public void setAtivo(boolean ativo) {
 		this.ativo = ativo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 }
